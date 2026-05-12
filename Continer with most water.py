fe1deee5eedaf6581cff6577e9251d6ee95570ab
akash <akashvpm440@gmail.com>
2026-05-12 22:58:43 +0530

    Continer with most water

--- Continer with most water.py
+++ Continer with most water.py
@@ -23,3 +23,22 @@ class Solution:
                 if cap > max: 
                     max = cap
         return max
+
+    def maxAreaOptimalMethod(self, height: List[int]) -> int:
+        maximum = 0
+        
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+
+            area = min(height[left], height[right]) * (right - left )
+
+            maximum = max(maximum, area)
+
+            if height[left] < height[right]:
+                left += 1 
+            else:
+                right -=1
+                
+        return maximum
