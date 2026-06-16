156734d8331089086f5adae2ef195a83ca45c8ba
akash <akashvpm440@gmail.com>
2026-06-16 22:29:48 +0530

    Fix indentation and formatting in maxProfit methods for consistency

--- righht_time_to_buy_sell_stock.py
+++ righht_time_to_buy_sell_stock.py
@@ -1,14 +1,29 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        l = 0
-        profit = 0
+      def maxProfit(self, prices: List[int]) -> int:
+            l = 0
+            profit = 0
 
-        for i in range(len(prices)): 
-            a = prices[l] - prices[i]
+            for i in range(len(prices)): 
+                  a = prices[l] - prices[i]
+
+                  if a > 0: 
+                        l = i
+                  if a < 0: 
+                        profit = max(profit, abs(a))
+
+            return profit
+
+      def maxProfit_2(self, prices: List[int]) -> int:
+            l = 0
+            profit = 0
+
+            for i in range(len(prices)): 
+
+                  if prices[l] > prices[i]: 
+                        l = i
+                        continue
+
+                  a = prices[i] - prices[l]
+                  profit = max(profit, a)
             
-            if a > 0: 
-                l = i
-            if a < 0: 
-                profit = max(profit, abs(a))
-        
-        return profit
+            return profit
