ea01d547a1aef247928f6c7181fc06a48e878aa7
akash <akashvpm440@gmail.com>
2026-06-16 23:06:26 +0530

    Refactor twoSum method to include an alternative implementation using sorting and two pointers

--- 2sum.py
+++ 2sum.py
@@ -9,3 +9,25 @@ class Solution:
                 return [seen[remain],i] 
             
             seen[num] = i
+            
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        out = []
+        arr = [(ind, num) for num, ind in enumerate(nums)]
+        arr.sort()
+
+        l = 0
+        r = len(arr)-1
+
+        while l < r:
+            if arr[r][0] + arr[l][0] == target: 
+                out.append(arr[r][1])
+                out.append(arr[l][1])
+                break
+
+            elif arr[r][0] + arr[l][0] < target:
+                l += 1
+
+            else: 
+                r -= 1
+
+        return out 
