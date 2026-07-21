a137232bd448dcadadf446a610b5952166530662
akash <akashvpm440@gmail.com>
2026-07-22 00:06:49 +0530

    Squrt(x)_BInnary_search_solution

--- squrt(x).py
+++ squrt(x).py
@@ -3,3 +3,24 @@ import math
 class Solution:
     def mySqrt(self, x: int) -> int:
         return int(math.sqrt(x))
+      
+    def mySqrt_Binary_search(self, x: int) -> int:
+
+        if x < 2: return x 
+
+        out = 0
+        
+        left = 0 
+        right = x 
+
+        while left <= right: 
+            mid = (left + right) // 2
+            if mid * mid == x: 
+                return int(mid)
+            elif mid * mid < x:
+                out = mid  
+                left = mid + 1
+            else: 
+                right = mid -1
+        
+        return int(out) 
